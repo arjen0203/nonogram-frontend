836fa685f93bd8d084e71a5e44b6fe7363ad92fb
Arjen <arjenmeer@hotmail.nl>
2020-05-12 21:09:58 +0200

    now gets the hints

--- src/Creating/Creating.js
+++ src/Creating/Creating.js
@@ -11,7 +11,9 @@ class Creating extends React.Component {
             width: 10,
             height: 10,
             name: '',
-            pictureGrid: this.getPictureGrid(10, 10)
+            pictureGrid: this.getPictureGrid(10, 10),
+            topHints: [],
+            sideHints: []
         };
     }
 
@@ -56,10 +58,57 @@ class Creating extends React.Component {
         let pictureGrid = this.getPictureGrid(this.state.width, height);
         this.setState({height, pictureGrid});
     }
+
     changeName(name) {
         let pictureGrid = this.getPictureGrid(this.state.width, this.state.height);
         this.setState({name, pictureGrid});
     }
+
+    setHints(){
+        let sideHints = [];
+        let counter = 0;
+
+        for (let x = 0; x < this.state.width; x++){
+            sideHints.push([]);
+            for (let y = 0; y < this.state.height; y++){
+                if (this.state.pictureGrid[x][y] === 1) counter++;
+                else if (counter > 0) {
+                    sideHints[x].push(counter);
+                    counter = 0;
+                }
+            }
+            if (counter !== 0 || sideHints[x].length === 0) {
+                sideHints[x].push(counter);
+                counter = 0;
+            }
+        }
+        console.log(sideHints);
+
+        let topHints = [];
+        counter = 0;
+
+        for (let y = 0; y < this.state.height; y++){
+            topHints.push([]);
+            for (let x = 0; x < this.state.width; x++){
+                if (this.state.pictureGrid[x][y] === 1) counter++;
+                else if (counter > 0) {
+                    topHints[y].push(counter);
+                    counter = 0;
+                }
+            }
+            if (counter !== 0 || topHints[y].length === 0) {
+                topHints[y].push(counter);
+                counter = 0;
+            }
+        }
+
+        this.setState({topHints, sideHints})
+    }
+
+    saveNonogram(){
+        this.setHints();
+    }
+
 
     render() {
         return (
@@ -67,12 +116,10 @@ class Creating extends React.Component {
                 <h1>
                     This is the creating page!
                 </h1>
-                <form>
                     <Options changeWidth={(e) => this.changeWidth(e)} changeHeight={(e) => this.changeHeight(e)} changeName={(e) => this.changeName(e)}
-                             width={this.state.width} height={this.state.height} name={this.state.name} ></Options>
+                             width={this.state.width} height={this.state.height} name={this.state.name} saveNonogram={() => this.saveNonogram()}></Options>
                     <DrawingGrid width={this.state.width} height={this.state.height} pictureGrid={this.state.pictureGrid}
                     onClick={(x, y, e) => this.onClick(x, y, e)} onDrag={(x, y, e) => this.onDrag(x, y, e)}></DrawingGrid>
-                </form>
             </div>
         );
     }
