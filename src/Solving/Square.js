42d25aee878041b07f6e860f39737e71fec9ddc5
Arjen <arjenmeer@hotmail.nl>
2020-05-19 10:46:23 +0200

    changed color scheme

--- src/Solving/Square.js
+++ src/Solving/Square.js
@@ -3,15 +3,15 @@ import React from "react";
 class Square extends React.Component {
     render() {
         var display;
+        var styling = "square";
         if (this.props.value === 1){
-            var filled = { backgroundColor: '#35393C'};
+            styling = "square-filled";
         } else if (this.props.value === 2){
             display = "✖";
         }
         return (
             <div
-                className={"square"}
-                style={filled}
+                className={styling}
                 onMouseDown={(e) => this.props.onMouseDown(e)}
                 onContextMenu={(e) => this.props.onContextMenu(e)}
                 onMouseEnter={(e) => this.props.onMouseEnter(e)}
