09fd3c9af825be662a5ea05f14d71cc87320427b
Arjen <arjenmeer@hotmail.nl>
2020-05-27 16:30:23 +0200

    Added simple registration

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -3,18 +3,70 @@ import {Link} from "react-router-dom";
 import './RegisterStyle.scss';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "",
+            password: "",
+            passwordRepeat: ""
+        }
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handlePasswordRepChange = this.handlePasswordRepChange.bind(this);
+        this.tryRegistrating = this.tryRegistrating.bind(this);
+    }
+
+    tryRegistrating(){
+        console.log("works");
+        this.saving = true;
+
+        let user = { username: this.state.username, password: this.state.password };
+        console.log(JSON.stringify(user));
+        fetch(`http://localhost:8080/api/user/add`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user)
+        })
+            .then(res => {
+                if(!res.ok){
+                    this.setState({ ...this.state, registerError: "Something went wrong while registrating"});
+                    this.saving = false;
+                    return Promise.reject("Promise rejected")
+                }
+                res.json()
+            })
+            .then(data => {
+                this.props.history.push("/");
+                this.saving = false;
+            })
+            .catch(this.setState({ ...this.state, registerError: "Something went wrong while registrating"}));
+    }
+
+    handleNameChange(event){
+        this.setState({username: event.target.value});
+    }
+
+    handlePasswordChange(event){
+        this.setState({password: event.target.value});
+    }
+    handlePasswordRepChange(event){
+        this.setState({passwordRepeat: event.target.value});
+    }
+
     render() {
         return (
             <div className="center">
                 <div className="register-fields">
                     <b className="register-title">Register:</b>
                     <label>Username:</label>
-                    <input className="register-username-input" type="text" placeholder=" Username"></input>
+                    <input className="register-username-input" type="text" placeholder=" Username" value={this.state.username} onChange={this.handleNameChange}></input>
                     <label>Password:</label>
-                    <input className="register-password-input" type="password" placeholder=" Password"></input>
+                    <input className="register-password-input" type="password" placeholder=" Password" value={this.state.password} onChange={this.handlePasswordChange}></input>
                     <label>Password repeat:</label>
-                    <input className="register-password-input" type="password" placeholder=" Password repeat"></input>
-                    <button className="register-submit-button">Register</button>
+                    <input className="register-password-input" type="password" placeholder=" Password repeat" value={this.state.passwordRepeat} onChange={this.handlePasswordRepChange}></input>
+                    <button className="register-submit-button" onClick={this.tryRegistrating}>Register</button>
 
                     <Link className="to-login-link" to={'/login'}>Already have an account? Login here.</Link>
                 </div>
