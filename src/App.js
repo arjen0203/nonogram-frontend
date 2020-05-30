b483555642e606312e614fea7be65b6c2bb7a510
Arjen <arjenmeer@hotmail.nl>
2020-05-30 12:47:20 +0200

    possibly working automatic login and login

--- src/App.js
+++ src/App.js
@@ -20,27 +20,63 @@ class App extends React.Component {
         const tokenDate = new Date(localStorage.getItem("tokenTimestamp"));
         const today = new Date();
 
+        const GuestUser = {userId: 0, username: "Guest", getToken: function () {return null;}};
+
         if (tokenDate === null | localStorage.getItem("token") === null) {
-            return {userId: 0, username: "Guest", getToken: function () {return null;}}
+            return GuestUser;
         }
         if (today.getDate() - tokenDate.getDate() > 7) {
             localStorage.removeItem("tokenTimestamp");
             localStorage.removeItem("token");
-            return {userId: 0, username: "Guest", getToken: function () {return null;}}
+            return GuestUser;
         } else {
             localStorage.setItem('date', today);
-            return this.getUserInfo();
+            let user = this.getUserInfo();
+            if (user === null) return GuestUser;
         }
-        return {userId: 0, username: "Guest", getToken: function () {return null;}}
+        return GuestUser;
     }
 
-    getUserInfo(){
-        return null;
+    async getUserInfo() {
+        let user;
+        await fetch('https://localhost:8080/api/user/me', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem("token")}
+        })
+            .then( async res => {
+                if (!res.ok) throw new Error(await res.text());
+                return res.json();
+            })
+            .then(data => {
+                user = {userId: this.data.id, username: this.data.username, getToken: function () {return localStorage.getItem("token")}};
+            }).catch(error => {
+            console.error('Couldnt get user', error);
+            return null;
+        });
+        return user;
     }
+
+    async loginUser(token) {
+        await localStorage.setItem("token");
+        this.setState({user: this.getUserInfo()});
+    }
+
+    logoutUser() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("tokenTimestamp")
+        this.setState( {user: this.getUserInfo()});
+    }
+
 
     render() {
+        const value = {
+            user: this.state.user,
+            logoutUser: this.logoutUser(),
+            loginUser: this.loginUser()
+        }
+
       return (
-          <UserContext.Provider value={this.state.user}>
+          <UserContext.Provider value={value}>
               <Router className="router">
                   <nav>
                       <ul className="router-list">
